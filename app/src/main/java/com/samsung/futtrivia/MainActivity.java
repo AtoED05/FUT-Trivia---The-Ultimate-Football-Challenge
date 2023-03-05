1a897e1445db9889641714d46b34068d35e20405
Eduard Atoyan <eduard.atoyan05@gmail.com>
2023-03-05 10:58:58 +0400

    launching, no splash, new icons

--- app/src/main/java/com/samsung/futtrivia/MainActivity.java
+++ app/src/main/java/com/samsung/futtrivia/MainActivity.java
@@ -1,10 +1,12 @@
 package com.samsung.futtrivia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.transition.TransitionManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MotionLayout motionLayout = findViewById(R.id.motionLayout);
+        TransitionManager.beginDelayedTransition(motionLayout);
 
         new Handler().postDelayed(new Runnable() {
             @Override
