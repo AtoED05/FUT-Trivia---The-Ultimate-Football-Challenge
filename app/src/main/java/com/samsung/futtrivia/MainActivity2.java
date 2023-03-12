89db5808943d604f51509690b99b58b851e5ef00
Eduard Atoyan <eduard.atoyan05@gmail.com>
2023-03-12 13:55:31 +0400

    working

--- app/src/main/java/com/samsung/futtrivia/MainActivity2.java
+++ app/src/main/java/com/samsung/futtrivia/MainActivity2.java
@@ -1,7 +1,11 @@
 package com.samsung.futtrivia;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -11,6 +15,18 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
     }
 
+    private static final int TIME_INTERVAL = 2000;
+    private long backPressedTime;
+
     @Override
-    public void onBackPressed(){}
+    public void onBackPressed() {
+        if (backPressedTime + TIME_INTERVAL > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finishAffinity();
+            return;
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        backPressedTime = System.currentTimeMillis();
+    }
 }
